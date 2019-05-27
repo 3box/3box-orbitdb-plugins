814715349f7bd3fa1687c9c38be3c0da1218628c
Joel Torstensson <oed3933@gmail.com>
2019-05-27 14:46:36 +0200

    feat: add access controller stumps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 const OdbIdentityProvider = require('./odbIdentityProvider')
+const ModeratorAccessController = require('./moderatorAccessController')
+const ThreadAccessController = require('./threadAccessController')
+const LegacyIPFS3BoxAccessController = require('./legacyIpfs3boxAccessController')
 
-module.exports = { OdbIdentityProvider }
+module.exports = { OdbIdentityProvider, ModeratorAccessController, ThreadAccessController, LegacyIPFS3BoxAccessController  }
